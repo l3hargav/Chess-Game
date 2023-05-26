a9ebb335618959d05264a0865e1ba0d582cc456c
Rajsekar Balaji <brj3raj@gmail.com>
2023-05-26 23:09:54 +0530

    Helper Functions

---
+++ game/src/helperFunctions/InitializeChessboard.js
@@ -0,0 +1,51 @@
+import King from "../pieces/King.js"
+import Queen from "../pieces/King.js"
+import Bishop from "../pieces/King.js"
+import Knight from "../pieces/King.js"
+import Rook from "../pieces/King.js"
+import Pawn from "../pieces/Pawn.js"
+
+/*
+    For player: 
+        player == 0 => "white"
+        player == 1 => "black"
+*/
+
+
+export default function InitializeChessboard() {
+    const board = Array(64).fill(null);
+
+    //Set Pawns
+    for(let i = 8; i < 16; i++){
+        squares[i] = new Pawn(0);
+        squares[i+40] = new Pawn(1);
+    }
+
+    //Set Rooks
+    board[0] = new Rook(0);
+    board[7] = new Rook(0);
+    board[56] = new Rook(1);
+    board[63] = new Rook(1);
+
+    //Set Knights
+    board[1] = new Knight(0);
+    board[6] = new Knight(0);
+    board[57] = new Knight(1);
+    board[62] = new Knight(1);
+
+    //Set Bishops
+    board[2] = new Bishop(0);
+    board[5] = new Bishop(0);
+    board[58] = new Bishop(1);
+    board[61] = new Bishop(1);
+
+    //Set Queens
+    board[3] = new Queen(0);
+    board[59] = new Queen(1);
+
+    //Set Kings
+    board[4] = new King(0);
+    board[60] = new King(1);
+
+    return board;
+}
