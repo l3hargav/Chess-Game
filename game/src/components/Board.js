b5ecdbb21ecbb74890817d0f2c19166e95bb7d51
Rajsekar Balaji <brj3raj@gmail.com>
2023-05-27 08:25:11 +0530

    Implemented Chessboard

--- game/src/components/Board.js
+++ game/src/components/Board.js
@@ -1,9 +1,20 @@
 import "../App.css"
-export default function Board() {
+import Tile from "./Tile.js"
+export default function Board(props) {
+    const board = []
+    for(let i = 0; i < 8; i++){
+        for (let j = 0; j < 8; j++){
+            let shade = "white";
+            let img = null;
+            if ((i+j) % 2 == 0){
+                shade = "black";
+            }
+            board.push(<Tile color={shade}/>)
+        }
+    }
     return(
-        <div className="container">
-            <div className="board">
-            </div>
+        <div className="board">
+            {board}
         </div>
     )
 }
