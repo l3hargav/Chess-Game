a00bf2b9c2673eae3a56dd55d6957990ad1a0e49
Rajsekar Balaji <brj3raj@gmail.com>
2023-05-27 08:25:51 +0530

    Added black and white color

--- game/src/components/Tile.js
+++ game/src/components/Tile.js
@@ -2,7 +2,7 @@ import "../App.css"
 export default function Tile(props) {
     return (
         <div className={props.color}>
-            <img src={props.piece.image} />
+
         </div>
     )
 }
