db7f0f33ee66708cdc10823aeea9a8b66edae339
Rajsekar Balaji <brj3raj@gmail.com>
2023-05-31 20:20:28 +0530

    Helper Function to initialize the starting position

--- game/src/helperFunctions/InitializeChessboard.js
+++ game/src/helperFunctions/InitializeChessboard.js
@@ -1,8 +1,8 @@
 import King from "../pieces/King.js"
-import Queen from "../pieces/King.js"
-import Bishop from "../pieces/King.js"
-import Knight from "../pieces/King.js"
-import Rook from "../pieces/King.js"
+import Queen from "../pieces/Queen.js"
+import Bishop from "../pieces/Bishop.js"
+import Knight from "../pieces/Knight.js"
+import Rook from "../pieces/Rook.js"
 import Pawn from "../pieces/Pawn.js"
 
 /*
@@ -17,10 +17,9 @@ export default function InitializeChessboard() {
 
     //Set Pawns
     for(let i = 8; i < 16; i++){
-        squares[i] = new Pawn(0);
-        squares[i+40] = new Pawn(1);
+        board[i] = new Pawn(0);
+        board[i+40] = new Pawn(1);
     }
-
     //Set Rooks
     board[0] = new Rook(0);
     board[7] = new Rook(0);
@@ -46,6 +45,6 @@ export default function InitializeChessboard() {
     //Set Kings
     board[4] = new King(0);
     board[60] = new King(1);
-
+ 
     return board;
 }
