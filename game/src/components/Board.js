7b833a0ee46e028af0d3e471771a9d0329023b17
Rajsekar Balaji <brj3raj@gmail.com>
2023-05-31 20:21:54 +0530

    Implemented rendering pieces

--- game/src/components/Board.js
+++ game/src/components/Board.js
@@ -1,15 +1,18 @@
 import "../App.css"
 import Tile from "./Tile.js"
+
 export default function Board(props) {
     const board = []
+    let index = 0;
     for(let i = 0; i < 8; i++){
         for (let j = 0; j < 8; j++){
             let shade = "white";
-            let img = null;
-            if ((i+j) % 2 == 0){
+            
+            if ((i+j) % 2 === 0){
                 shade = "black";
             }
-            board.push(<Tile color={shade}/>)
+            board.push(<Tile color={shade} onClick={props.onClick} src={props.squares[index] ? props.squares[index].image : null} />)
+            index += 1;
         }
     }
     return(
