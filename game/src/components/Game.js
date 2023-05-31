4b82204217a9cf29065a553396d56d4b9a371807
Rajsekar Balaji <brj3raj@gmail.com>
2023-05-31 20:29:10 +0530

    Implemented board state

--- game/src/components/Game.js
+++ game/src/components/Game.js
@@ -2,7 +2,7 @@
 // IMPLEMENTS ALL THE COMPONENTS AND GAME LOGIC 
 
 import "../App.css";
-//import { useState } from "react"
+import { useState } from "react"
 import Board from "./Board.js"
 import InitializeChessboard from "../helperFunctions/InitializeChessboard.js"
 
@@ -18,11 +18,16 @@ function handleClick(piece) {
 
 export default function Game() {
         // TODO: IMPLEMENT LOGIC
-        let board = InitializeChessboard();
+        const [state, setState] = useState({
+            board: InitializeChessboard(),
+            player: 0,    // Player whose move it is. 0 => "WHITE" : 1 => "BLACK"
+            sourceSelection: -1,    // -1 until a Piece is selected. If a Piece is selected then value is the index in squares
+            turn: "white"
+        })
         return (
             <div>
                 <div className="game-board">
-                    <Board squares={board} />
+                    <Board board={state.board} />
                 </div>
             </div>
         )
