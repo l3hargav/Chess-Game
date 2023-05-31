ee8d3d56531e6397657834063a46c72c64ef1557
Rajsekar Balaji <brj3raj@gmail.com>
2023-05-31 20:21:31 +0530

    Implemented rendering pieces

--- game/src/components/Tile.js
+++ game/src/components/Tile.js
@@ -2,7 +2,7 @@ import "../App.css"
 export default function Tile(props) {
     return (
         <div className={props.color}>
-
+            <img id="pieces" src={props.src} />
         </div>
     )
 }
