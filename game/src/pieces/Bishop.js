8ecdb9b8506c2121400dbc9d4f92805a3452511d
Rajsekar Balaji <brj3raj@gmail.com>
2023-06-02 22:32:09 +0530

    Implemented check for move and a function to return path

--- game/src/pieces/Bishop.js
+++ game/src/pieces/Bishop.js
@@ -4,7 +4,40 @@ export default class Bishop extends Piece {
     constructor(player){
         super(
             player,
-            (player == 0 ? "images/bishop_white.jpg" : "images/bishop_black.jpg")
+            (player === 0 ? "images/bishop_white.jpg" : "images/bishop_black.jpg")
         );
+    }
+
+    //CHECKS IF GIVEN MOVE IS POSSIBLE
+    isMovePossible(curr, dest) {
+        return (
+            Math.abs(curr - dest) % 9 === 0 || Math.abs(curr - dest) % 7 === 0
+        )
+    }
+
+    //GETS PATH THAT THE PIECE HAS TO TRAVEL
+    getPath(curr, dest) {
+        let path = []
+        let start, end, increment;
+        if(curr > dest) {
+            start = dest;
+            end = curr;
+        }
+        else {
+            start = curr;
+            end = dest;
+        }
+        if(Math.abs(curr - dest) % 9 === 0) {
+            increment = 9;
+            start += 9;
+        }
+        else {
+            increment = 7;
+            start += 7;
+        }
+        for (let i = start; i < end; i+=increment) {
+            path.push(i);
+        }
+        return path;
     }
 }
--- game/src/pieces/King.js
+++ game/src/pieces/King.js
@@ -4,7 +4,29 @@ export default class King extends Piece {
     constructor(player){
         super(
             player,
-            (player == 0 ? "images/king_white.jpg" : "images/king_black.jpg")
+            (player === 0 ? "images/king_white.jpg" : "images/king_black.jpg")
         );
+    }
+    //CHECKS IF GIVEN MOVE IS POSSIBLE
+    isMovePossible(curr, dest) {
+        return (
+            //Left and right
+            curr - 1 === dest ||
+            curr + 1 === dest ||
+            //Top and bottom
+            curr + 8 === dest ||
+            curr - 8 === dest ||
+            //Top left and bottom left
+            curr + 7 === dest ||
+            curr - 9 === dest ||
+            //Top right and bottom right
+            curr + 9 === dest ||
+            curr - 7 === dest
+            )
+    }
+
+    //GETS PATH THAT THE PIECE HAS TO TRAVEL
+    getPath(curr, dest) {
+        return [] //Empty because destination is adjacent tile
     }
 }
