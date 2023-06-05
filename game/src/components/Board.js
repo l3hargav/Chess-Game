a7d8dc29599f43632f2af67af7a369a8aedb970f
Rajsekar Balaji <brj3raj@gmail.com>
2023-06-05 22:45:02 +0530

    Minor syntax correction

--- game/src/components/Board.js
+++ game/src/components/Board.js
@@ -3,6 +3,8 @@ import Tile from "./Tile.js"
 
 export default function Board(props) {
     let board = []
+    let index = 0;
+    let i = 0;
     for(let i = 0; i < 8; i++){
         for (let j = 0; j < 8; j++){
             let shade = "white";
@@ -10,7 +12,8 @@ export default function Board(props) {
             if ((i+j) % 2 === 0){
                 shade = "black";
             }
-            board.push(<Tile color={shade} onClick={props.onClick} src={props.board[index] ? props.board[index].image : null} />)
+            board.push(<Tile color={shade} index={index} onClick={() => props.onClick((i*8)+j)} src={props.board[index] ? props.board[index].image : null} />)
+            index += 1
         }
     } 
     return(
