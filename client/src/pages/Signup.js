bc10e93f9fc9526eea323cd1df56f625dc8e55ce
Rajsekar Balaji <brj3raj@gmail.com>
2023-06-26 22:31:07 +0530

    New use signup page which also adds entry to database

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,11 +1,33 @@
+import "../App.css"
+
+import { useNavigate } from "react-router-dom";
 export default function Signup() {
-    function handleSubmit() {
+    let navigate = useNavigate();
+    function handleSubmit(e) {
+        e.preventDefault();
         console.log("Submitted info")
+        let name = e.target[0].value;
+        let pass = e.target[1].value;
+
+        fetch("/api/newUser", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: name,
+                password: pass
+            })
+        })
+        alert("Successfully Signed in. Redirecting to login page...");
+        navigate("/login");  
+
     }
     return (
-        <div id="homepage">
+        <div id="mainpage">
             <img src="images/king_white.jpg"/>
             <h1>Chess Game</h1>
+            <h2>Signup form</h2>
             <img src = "images/king_white.jpg" />
             <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="Enter username">
