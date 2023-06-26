5bfd67f7b264f8b4f18cb8a7423b5dbd18ad9d50
Rajsekar Balaji <brj3raj@gmail.com>
2023-06-26 22:32:48 +0530

    Added routes and also added socket context

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import './App.css';
-import { useEffect, useState } from 'react';
 import Game from "./components/Game.js"
 import Signup from "./pages/Signup.js"
+import Main from "./pages/Main.js"
+import Login from "./pages/Login.js"
+import Home from "./pages/Home.js"
+import { socket, SocketContext } from "./socket.js";
+
+import { Routes, Route } from "react-router-dom"
 function App() {
   return (
-    <div id="home">
-      <Signup />
-    </div>
-    /*
+    <SocketContext.Provider value={socket}>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/user/:id" element={<Home />} /> 
+        <Route path="/game/:id" element={<Game />} />
+      </Routes>
+    </SocketContext.Provider>
+    /*    
     <div id="app">
       <Game />
     </div>
